45925ef91b78d5ab2e3cb62604bc77d4070f419e
slowstarter80 <slowstarter80@daum.net>
2021-07-29 13:22:00 +0900

    implement image inversion function using opencv

--- src/lib/image_processing_opencv.h
+++ src/lib/image_processing_opencv.h
@@ -14,12 +14,14 @@
 
 #include <gtkmm.h>
 #include "image_processing_main.h"
+#include <opencv2/opencv.hpp>
 
 class ImageProcessingOpenCv: public ImageProcessingMain
 {
 	public:
 		virtual ~ImageProcessingOpenCv();
  		virtual int image_processing_handler(magnolia_cmd_type *cmd, Gdk::Pixbuf &src_img, Gdk::Pixbuf &dst_img);
+		cv::Mat convert_gdk_pixbuf_to_cv_mat(Gdk::Pixbuf &src_img);
 	protected:
 	private:
 
